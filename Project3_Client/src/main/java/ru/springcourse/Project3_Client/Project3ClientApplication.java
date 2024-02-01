d630f565691c43ec259345076d8ad703fdf58f2d
EgorBystrov <156329541+EgorBystrov@users.noreply.github.com>
2024-02-01 08:09:20 +0300

    Add files via upload

---
+++ Project3_Client/src/main/java/ru/springcourse/Project3_Client/Project3ClientApplication.java
@@ -0,0 +1,13 @@
+package ru.springcourse.Project3_Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3ClientApplication.class, args);
+	}
+
+}
---
+++ Project3_Client/src/test/java/ru/springcourse/Project3_Client/Project3ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.springcourse.Project3_Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
